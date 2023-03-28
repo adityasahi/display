6f7bb60ae1375e2e0b5015ec1778342680ecd2c2
Aditya Sahi <as301518@ohio.edu>
2023-03-28 09:20:21 -0400

    bitmap code updated

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,9 @@
 #include <SPI.h>
 #include "Adafruit_GFX.h"
 #include "Adafruit_RA8875.h"
-#include <SdFat.h>
+#include <SD.h>
+#include <Adafruit_ImageReader.h>
+
 
 #define RA8875_INT 3
 #define RA8875_CS 10
@@ -9,121 +11,106 @@
 
 Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS, RA8875_RESET);
 
-SdFat sd;
-
 uint16_t tx, ty;
-int selectedOption = 0;
+int menuIndex = 0; // current menu selection
 int maxOption = 3;
 bool inSettings = false;
 int brightness = 255;
+int selectedOption = 0;
 
 void setup() {
-  Serial.begin(9600);
-  Serial.println("RA8875 start");
-
-  /* Initialize the display */
-  if (!tft.begin(RA8875_800x480)) {
-    Serial.println("RA8875 Not Found!");
-    while (1);
-  }
-
-  /* Initialize the SD card */
-  if (!sd.begin(4)) {
-    Serial.println("SD card initialization failed!");
-    while (1);
-  }
-
-  tft.displayOn(true);
-  tft.GPIOX(true);      // Enable TFT - display enable tied to GPIOX
-  tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // PWM output for backlight
-  tft.PWM1out(brightness);
-  tft.fillScreen(RA8875_BLACK);
-
-  /* Switch to text mode */
+  tft.begin(RA8875_800x480); // initialize the display
+  tft.displayOn(true); // turn on the display
+  tft.GPIOX(true); // enable the backlight
+  tft.PWM1config(true, RA8875_PWM_CLK_DIV1024); // set the backlight brightness
+  tft.PWM1out(255); // set the backlight brightness to maximum */
   tft.textMode();
   tft.cursorBlink(32);
 }
 
-void drawmenu() {
-  tft.fillScreen(RA8875_BLACK);
+
+void drawMenu() {
+  tft.fillScreen(RA8875_BLACK); // clear the screen
   tft.textMode();
-  tft.textSetCursor(10, 10);
-  tft.textEnlarge(2);
-  tft.textWrite("Menu:");
-
-  tft.textEnlarge(1);
-  tft.textSetCursor(10, 40);
-  if (selectedOption == 0) {
-    tft.textWrite(">");
-  } else {
-    tft.textWrite(" ");
+  tft.textSetCursor(200, 100); // set the cursor position
+  tft.textColor(RA8875_WHITE,RA8875_BLACK); // set the text color to white
+  tft.setTextSize(3); // set the text size to 3
+ 
+  if (menuIndex == 0) {
+    tft.textWrite("> Select Game\n"); // highlight selected option
+    tft.textWrite("  Demo\n");
+    tft.textWrite("  Settings\n");
   }
-  tft.textWrite("Game Select");
-
-  tft.textSetCursor(10, 60);
-  if (selectedOption == 1) {
-    tft.textWrite(">");
-  } else {
-    tft.textWrite(" ");
+  else if (menuIndex == 1) {
+    tft.textWrite("  Select Game\n");
+    tft.textWrite("> Demo\n"); // highlight selected option
+    tft.textWrite("  Settings\n");
   }
-  tft.textWrite("Demo");
-
-  tft.textSetCursor(10, 80);
-  if (selectedOption == 2) {
-    tft.textWrite(">");
-  } else {
-    tft.textWrite(" ");
+  else if (menuIndex == 2) {
+    tft.textWrite("  Select Game\n");
+    tft.textWrite("  Demo\n");
+    tft.textWrite("> Settings\n"); // highlight selected option
   }
-  tft.textWrite("Settings");
-
-  tft.textEnlarge(2);
 }
 
+
+void handleInput() {
+  if (digitalRead(0) == LOW) { // button A pressed
+    menuIndex--;
+    if (menuIndex < 0) {
+      menuIndex = 2; // wrap around to end of menu
+    }
+  }
+  else if (digitalRead(1) == LOW) { // button B pressed
+    menuIndex++;
+    if (menuIndex > 2) {
+      menuIndex = 0; // wrap around to beginning of menu
+    }
+  }
+  else if (digitalRead(2) == LOW) { // button C pressed
+    // do something based on selected option
+    if (menuIndex == 0) {
+      // Select Game selected
+      tft.fillScreen(RA8875_BLACK); // clear the screen
+      tft.textMode();
+      tft.textSetCursor(200, 100); // set the cursor position
+      tft.textColor(RA8875_WHITE,RA8875_BLACK); // set the text color to white
+      tft.setTextSize(3); // set the text size to 3
+      tft.textWrite("Select Game"); // display selected option
+      delay(1000); // wait for 1 second
+      drawMenu(); // redraw menu
+    }
+    else if (menuIndex == 1) {
+      // Demo selected
+      tft.fillScreen(RA8875_BLACK); // clear the screen
+      tft.textMode();
+      tft.textSetCursor(200, 100); // set the cursor position
+      tft.textColor(RA8875_WHITE,RA8875_BLACK); // set the text color to white
+      tft.setTextSize(3); // set the text size to 3
+      tft.textWrite("Demo"); // display selected option
+      delay(1000); // wait for 1 second
+      drawMenu(); // redraw menu
+    }
+    else if (menuIndex == 2) {
+      // Settings selected
+      tft.fillScreen(RA8875_BLACK); // clear the screen
+      tft.textMode();
+      tft.textSetCursor(200, 100); // set the cursor position
+      tft.textColor(RA8875_WHITE,RA8875_BLACK); // set the text color to white
+      tft.setTextSize(3); // set the text size to 3
+      tft.textWrite("Settings"); // display selected option
+      delay(1000); // wait for 1 second
+      drawMenu(); // redraw menu
+    }
+  }
+  else if (digitalRead(3) == LOW) { // button D pressed
+    // do something else
+  }
+}
+
+
 void loop() {
-  drawmenu();
-
-  while (!Serial.available()); // Wait for user input
-  int option = Serial.parseInt(); // Read user input
-
-  if (inSettings) {
-    if (option == 1) {
-      brightness = min(255, brightness + 25);
-      tft.PWM1out(brightness);
-    } else if (option == 2) {
-      brightness = max(0, brightness - 25);
-      tft.PWM1out(brightness);
-    } else if (option == 3) {
-      inSettings = false;
-    } else {
-      Serial.println("Invalid option.");
-    }
-  } else {
-    if (option == 1) {
-      /* Load and draw bitmap from SD card */
-      // tft.bmpDraw("bitmap.bmp", 0, 0);
-    } else if (option == 2) {
-      Serial.println("Demo selected");
-    } else if (option == 3) {
-      inSettings = true;
-    } else {
-      Serial.println("Invalid option.");
-    }
-  }
-
-  // Update selectedOption based on user input
-  if (option > 0 && option <= maxOption) {
-    selectedOption = option - 1;
-  }
-
-  // Wait for user input to go back or forward in menu
-  while (Serial.available()) Serial.read();
-  while (!Serial.available()) {
-    delay(50);
-  }
-  int input = Serial.read();
-  if (input == 'b' && selectedOption > 0) {
-    selectedOption--;
-  } else if (input == 'f' && selectedOption < maxOption - 1) {
-    selectedOption++;
-  }
+  drawMenu();
+  // handleInput();
+  delay(1000);
 }
